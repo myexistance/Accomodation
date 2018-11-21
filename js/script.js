b0e5e5d42a0fc988b0ecdf1f88a29e2f643e495f
myexistance <joonyoonlives@gmail.com>
2018-11-21 23:19:02 +1300

    tab contents alighned

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 //Date datepicker
 $(function(){
     $('#startDate').datepicker();
-
     $('#endDate').datepicker();
-
-});
+  });
 
 //submit.addEventListener('click',dateDiff);
-function dateDiff() {
-
-    var start  = $('#startDate').datepicker('getDate');
-   var end = $('#endDate').datepicker('getDate');
-   var days  = (end - start)/1000/60/60/24;
-   alert(days);
-   document.getElementById("noDays").innerHTML = days ;
-}
+// function dateDiff() {
+//
+//     var start  = $('#startDate').datepicker('getDate');
+//    var end = $('#endDate').datepicker('getDate');
+//    var days  = (end - start)/1000/60/60/24;
+//    alert(days);
+//    document.getElementById("noDays").innerHTML = days ;
+// }
+// $('#myTab a[href="#profile"]').tab('show') // Select tab by name
+// $('#myTab li:first-child a').tab('show') // Select first tab
+// $('#myTab li:last-child a').tab('show')
