c01f83cc9d7e1d0e19eb75985ac3a2fcabed1ce8
Hojoong Yoon <932400976@bun2-206-15.local>
2018-11-21 15:22:26 +1300

    carousel added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+//Date datepicker
+$(function(){
+    $('#startDate').datepicker();
+
+    $('#endDate').datepicker();
+
+});
+
+//submit.addEventListener('click',dateDiff);
+function dateDiff() {
+
+    var start  = $('#startDate').datepicker('getDate');
+   var end = $('#endDate').datepicker('getDate');
+   var days  = (end - start)/1000/60/60/24;
+   alert(days);
+   document.getElementById("noDays").innerHTML = days ;
+}
