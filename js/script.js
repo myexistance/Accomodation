543d714a519705c6dcf6eb67001aa5f0f2426bb7
Hojoong Yoon <932400976@bun2-206-15.local>
2018-11-22 14:51:20 +1300

    indication of user input

--- js/script.js
+++ js/script.js
@@ -4,18 +4,21 @@ $(function(){
     $('#endDate').datepicker();
   });
 
-//submit.addEventListener('click',dateDiff);
-// function dateDiff() {
-//
-//     var start  = $('#startDate').datepicker('getDate');
-//    var end = $('#endDate').datepicker('getDate');
-//    var days  = (end - start)/1000/60/60/24;
-//    alert(days);
-//    document.getElementById("noDays").innerHTML = days ;
-// }
-// $('#myTab a[href="#profile"]').tab('show') // Select tab by name
-// $('#myTab li:first-child a').tab('show') // Select first tab
-// $('#myTab li:last-child a').tab('show')
+//Find the number of days between dates
+function dateDiff() {
+
+   var start  = $('#startDate').datepicker('getDate');
+   var end = $('#endDate').datepicker('getDate');
+   var days  = (end - start)/1000/60/60/24;
+   alert(days);
+   document.getElementById("noDays").innerHTML = days + " Days   > " ;
+}
+
+function noGuests() {
+  var g = document.getElementById('noGuest').value;
+  document.getElementById("numGuest").innerHTML = g + " Guest(s)  " ;
+}
+// Jumping between tabs in the modal
 $(function () {
     $('#date a').click(function (e) {
         e.preventDefault();
