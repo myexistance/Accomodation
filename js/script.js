33f09c2a77d109522d1768cdd87ab6377573dfe5
Hojoong Yoon <932400976@bun2-206-15.local>
2018-11-27 15:30:45 +1300

    Tuesday 30th

--- js/script.js
+++ js/script.js
@@ -5,19 +5,21 @@ $(function(){
   });
 
 //Find the number of days between dates
-function dateDiff() {
+function calculate() {
 
    var start  = $('#startDate').datepicker('getDate');
    var end = $('#endDate').datepicker('getDate');
    var days  = (end - start)/1000/60/60/24;
    alert(days);
    document.getElementById("noDays").innerHTML = days + " Days   > " ;
+   var g = document.getElementById('noGuest').value;
+   document.getElementById("numGuest").innerHTML = g + " Guest(s)  " ;
 }
 
-function noGuests() {
-  var g = document.getElementById('noGuest').value;
-  document.getElementById("numGuest").innerHTML = g + " Guest(s)  " ;
-}
+// function noGuests() {
+//   var g = document.getElementById('noGuest').value;
+//   document.getElementById("numGuest").innerHTML = g + " Guest(s)  " ;
+// }
 // Jumping between tabs in the modal
 $(function () {
     $('#date a').click(function (e) {
@@ -35,3 +37,21 @@ $(function () {
         $('a[href="' + $(this).attr('href') + '"]').tab('show');
     })
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
